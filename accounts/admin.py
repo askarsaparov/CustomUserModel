5f049ed6a6c4328718eaa9cbf485136a9801fda2
askarsaparov <asqarsaparov2303@gmail.com>
2022-06-09 11:17:05 +0500

    model optimallashdi

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,11 +4,12 @@ from django.contrib.auth.admin import UserAdmin
 from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 from accounts.models import CustomUser
 
+admin.site.register(CustomUser)
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['username', 'email',]
+# class CustomUserAdmin(UserAdmin):
+#     add_form = CustomUserCreationForm
+#     form = CustomUserChangeForm
+#     model = CustomUser
+#     list_display = ['username', 'email',]
 
-admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,10 @@ from django.db import models
 
 class CustomUser(AbstractUser):
 
+    class Meta:
+        verbose_name = "CustomUser"
+        verbose_name_plural = "CustomUsers"
+
+
     def __str__(self):
         return self.username
